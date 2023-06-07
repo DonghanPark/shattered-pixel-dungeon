102d2b54cebd84263d72eb4d1ae1585b709fe2d6
jeong jin kyu <jjk3969@naver.com>
2023-06-07 09:17:12 +0900

    [Initial]: create CharTest.java

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/CharTest.java
@@ -0,0 +1,12 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharTest {
+
+    @Test
+    void onRemove() {
+    }
+}
